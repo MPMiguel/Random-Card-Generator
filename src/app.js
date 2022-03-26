01a0e188b6a3ac40ca4fde6a4405f6fa41531944
MPMiguel <martinezpazosma@gmail.com>
2022-03-26 20:31:30 +0000

    Estructura

--- src/app.js
+++ src/app.js
@@ -9,3 +9,15 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+//me llevo esto
+let i = 0;
+let num = 0;
+let numerosQueYaSalieron = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+while (i < 12) {
+  while (numerosQueYaSalieron[i - 1] == num) {
+    num = Math.floor(Math.random() * 12);
+  }
+  numerosQueYaSalieron[i] = num;
+  console.log(numerosQueYaSalieron[i]);
+  i++;
+}
